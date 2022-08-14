eabf7991c820bd3eb21c2fc9b946dcb206cf7e01
Bertrand Marron <bertrand.marron@gmail.com>
2022-08-14 18:51:51 +0200

    feat: create config

---
+++ README.md
@@ -0,0 +1,58 @@
+# eslint-config-xo-bizon
+
+> ESLint [shareable config](http://eslint.org/docs/developer-guide/shareable-configs.html) to be used with [xo](https://github.com/xojs/xo) or [eslint-config-xo](https://github.com/xojs/eslint-config-xo)
+
+[![npm version](https://badgen.net/npm/v/eslint-config-xo-bizon)](https://www.npmjs.com/package/eslint-config-xo-bizon)
+[![XO code style](https://badgen.net/badge/code%20style/XO/cyan)](https://github.com/xojs/xo)
+
+## CI
+
+[![Tests](https://github.com/bizon/eslint-config-xo-bizon/actions/workflows/tests.yml/badge.svg)](https://github.com/bizon/eslint-config-xo-bizon/actions/workflows/tests.yml)
+[![Release](https://github.com/bizon/eslint-config-xo-bizon/actions/workflows/release.yml/badge.svg)](https://github.com/bizon/eslint-config-xo-bizon/actions/workflows/release.yml)
+
+## Install
+
+```
+$ yarn add --dev eslint-config-xo-bizon
+```
+
+### Example usage with XO
+
+```json
+{
+  "name": "my-awesome-project",
+  "devDependencies": {
+    "xo": "*",
+    "eslint-config-xo-bizon": "*"
+  },
+  "xo": {
+    "extends": "xo-bizon",
+    "semicolon": false,
+    "space": 2
+  }
+}
+```
+
+
+## Related
+
+- [XO](https://github.com/xojs/xo)
+- [eslint-config-xo](https://github.com/xojs/eslint-config-xo) - ESLint shareable config for XO
+
+
+## License
+
+MIT © [Bizon](https://github.com/bizon)
+
+
+## Miscellaneous
+
+```
+    ╚⊙ ⊙╝
+  ╚═(███)═╝
+ ╚═(███)═╝
+╚═(███)═╝
+ ╚═(███)═╝
+  ╚═(███)═╝
+   ╚═(███)═╝
+```
---
+++ index.cjs
@@ -0,0 +1,79 @@
+module.exports = {
+  rules: {
+    // This reverts the "no null" decision from xo
+    '@typescript-eslint/ban-types': 'off',
+
+    // This would be nice in an ideal world, even warnings are too much
+    '@typescript-eslint/naming-convention': 'off',
+
+    // These rules are too restrictive
+    '@typescript-eslint/no-unsafe-argument': 'off',
+    '@typescript-eslint/no-unsafe-assignment': 'off',
+    '@typescript-eslint/no-unsafe-call': 'off',
+    '@typescript-eslint/no-unsafe-member-access': 'off',
+    '@typescript-eslint/no-unsafe-return': 'off',
+
+    // This rule is too restrictive
+    '@typescript-eslint/restrict-template-expressions': 'off',
+
+    // Let’s enable this when we switch to ESM
+    'import/extensions': 'off',
+
+    // Arbitrary import order rules
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'internal', ['parent', 'sibling'], 'index'],
+        pathGroups: [
+          {
+            pattern: '@/lib/**',
+            group: 'internal',
+            position: 'after',
+          },
+          {
+            pattern: '@/tests/**',
+            group: 'internal',
+            position: 'after',
+          },
+          {
+            pattern: '@/types/**',
+            group: 'internal',
+            position: 'after',
+          },
+          {
+            pattern: '@/!(lib|tests|types)/**',
+            group: 'internal',
+            position: 'after',
+          },
+        ],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
+    ],
+
+    // Let’s only warn one using reduce
+    'unicorn/no-array-reduce': 'warn',
+
+    // These rules are broken
+    'unicorn/no-array-callback-reference': 'off',
+    'unicorn/no-array-method-this-argument': 'off',
+
+    // Let’s not use ESM yet
+    'unicorn/prefer-module': 'off',
+
+    // This still fails with some libraries
+    'unicorn/prefer-node-protocol': 'off',
+
+    // This sorts named imports
+    'sort-imports': [
+      'error',
+      {
+        ignoreCase: true,
+        ignoreDeclarationSort: true,
+      },
+    ],
+  },
+}
